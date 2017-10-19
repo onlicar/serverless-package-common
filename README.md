dc6f6d26255b217232236fc3caec9d03b2432d16
Dan Bovey <dan@danbovey.uk>
2017-10-19 12:53:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# serverless-package-lib
+
+> 📦 Deploy Python Lambda functions with custom dependencies stored in a lib folder
+
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+
+class PackageLib {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = {
+      libFolder: options.libFolder || './lib'
+    };
+
+    this.hooks = {
+      'before:package:createDeploymentArtifacts': this.symlinkPackages.bind(this),
+      'after:deploy:deploy': this.removePackages.bind(this)
+    };
+  }
+
+  symlinkPackages() {
+    this.serverless.cli.log(`[serverless-lib-package] Symlinking packages`);
+    const libs = fs.readdirSync(this.options.libFolder);
+    libs.forEach(pkg => {
+      fs.symlinkSync(`${this.options.libFolder}/${pkg}`, path.join(process.cwd(), pkg));
+    });
+
+    process.on('SIGTERM', () => this.removePackages());
+  }
+
+  removePackages() {
+    const libs = fs.readdirSync('${this.options.libFolder}');
+    libs.forEach(pkg => {
+      rimraf.sync(path.join(process.cwd(), pkg));
+    });
+  }
+}
+
+module.exports = PackageLib;
